86ac8b237c3fa139fa6289ece0b14bb1ffa1166d
fury <poppunith2001@gmail.com>
2021-06-09 17:03:45 +0530

    added body

--- src/App.vue
+++ src/App.vue
@@ -61,13 +61,105 @@
         </div>
 
         <!-- main content -->
-        <div class="w-full h-full relative bg-blue-400">
+        <div class="w-full h-full relative overflow-y-scroll no-scrollbar">
           <!-- header -->
-          <div class="w-full sticky top-0 p-2"></div>
+          <div
+            class="w-full sticky top-0 px-5 py-3 z-10 flex items-center justify-between bg-black"
+          >
+            <div class="flex items-center">
+              <button
+                v-for="(btn,key) in arrowbuttons "
+                :key="key"
+                class="rounded-full focus:outline-none bg-black mr-2 w-9 h-9 text-white"
+              >
+                <i class="material-icons text-3xl">{{ btn.icon }}</i>
+              </button>
+            </div>
+            <div class="relative">
+              <button
+                @click="showDropDown = !showDropDown"
+                class="bg-light rounded-full p-1 focus:outline-none flex items-center"
+              >
+                <img
+                  src="https://cdn.quasar.dev/img/boy-avatar.png"
+                  alt
+                  class="rounded-full h-6 w-6"
+                />
+                <p class="text-white font-semibold text-xs ml-1 mr-2">Punith Fury</p>
+                <i v-if="showDropDown" class="material-icons text-white">arrow_drop_down</i>
+                <i v-if="!showDropDown" class="material-icons text-white">arrow_drop_up</i>
+              </button>
+              <div
+                @click="showDropDown = !showDropDown"
+                v-if="!showDropDown"
+                class="absolute bg-light w-full rounded mt-1"
+              >
+                <button
+                  class="text-center focus:outline-none text-sm w-full py-2 text-lightest border-b border-lightest opacity-75 hover:opacity-100 hover:text-white"
+                >Accounts</button>
+                <button
+                  class="text-center focus:outline-none text-sm w-full py-2 text-lightest border-b border-light hover:text-white opacity-75 hover:opacity-100"
+                >Log Out</button>
+              </div>
+            </div>
+          </div>
+          <!-- body -->
+          <div class="px-6 py-3">
+            <div class="flex items-center justify-between">
+              <h3
+                class="text-white text-2xl text-semibold tracking-wide hover:underline"
+              >Recently Played</h3>
+              <h3 class="text-xs pr-6 text-lightest tracking-wide hover:underline">See All</h3>
+            </div>
+            <div class="mt-3 w-full flex flex-wrap">
+              <div v-for="n in 8" :key="n" class="p-2 w-48 relative">
+                <div
+                  class="absolute w-full h-full flex items-end justify-end p-8 opacity-0 hover:opacity-100"
+                >
+                  <div class="bg-green rounded-full px-2 h-10 flex items-center justify-end">
+                    <i class="material-icons">play_arrow</i>
+                  </div>
+                </div>
+                <div class="bg-light w-full p-5 h-auto">
+                  <img src="./songp.jpg" class="h-auto w-full shadow mt-2" alt />
+                  <h3 class="text-sm font-semibold tracking-wide text-white mt-3">Love the Vue</h3>
+                  <h3 class="text-xs text-lightest tracking-wide">By Spotify</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="px-6 py-3">
+            <div>
+              <h3 class="text-white text-2xl text-semibold tracking-wide">Your Recommendations</h3>
+              <h3
+                class="text-sm text-lightest"
+              >Get better recommendations from listening to more songs</h3>
+            </div>
+            <div class="mt-3 w-full flex flex-wrap">
+              <div v-for="n in 8" :key="n" class="p-2 w-48 relative">
+                <div
+                  class="absolute w-full h-full flex items-end justify-end p-8 opacity-0 hover:opacity-100"
+                >
+                  <div class="bg-green rounded-full px-2 h-10 flex items-center justify-end">
+                    <i class="material-icons">play_arrow</i>
+                  </div>
+                </div>
+                <div class="bg-light w-full p-5 h-auto">
+                  <img src="./songp.jpg" class="h-auto w-full shadow mt-2" alt />
+                  <h3 class="text-sm font-semibold tracking-wide text-white mt-3">Love the Vue</h3>
+                  <h3 class="text-xs text-lightest tracking-wide">By Spotify</h3>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <!-- play Bar -->
-      <div class="w-full bg-light" style="height: 12h;"></div>
+      <div class="w-full bg-light flex justify-between px-3" style="height: 12h;">
+        <div>
+          
+        </div>
+      </div>
     </div>
     <div class="bg-dark h-screen block md:hidden">
       <div
@@ -77,8 +169,6 @@
   </div>
 </template>
 
-<script setup>
-</script>
 
 <style>
 </style>
@@ -99,6 +189,7 @@ export default {
         { id: "2", name: "Liked Songs", icon: "favorite" },
       ],
       setID: "home",
+      showDropDown: false,
       albums: [
         { name: "drive" },
         { name: "drive2" },
@@ -106,6 +197,10 @@ export default {
         { name: "drive4" },
         { name: "drive5" },
         { name: "drive6" },
+      ],
+      arrowbuttons: [
+        { icon: "keyboard_arrow_left" },
+        { icon: "keyboard_arrow_right" },
       ],
     };
   },
